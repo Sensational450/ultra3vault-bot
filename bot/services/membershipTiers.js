76a21d301b24345266474a0ebaab349add1cb85e
Ultra3 Bots <dennistyg82@gmail.com>
2026-05-30 16:06:50 +0200

    Create membershipTiers.js

---
+++ bot/services/membershipTiers.js
@@ -0,0 +1,11 @@
+module.exports = {
+    FREE: {
+        access: ["crypto-news"]
+    },
+    VIP: {
+        access: ["crypto-news", "alpha-news", "whale-alerts"]
+    },
+    ALPHA: {
+        access: ["crypto-news", "alpha-news", "whale-alerts", "security-alerts"]
+    }
+};
