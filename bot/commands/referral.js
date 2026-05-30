fc6b0d94ee6d20d95cd894108e932c23ff132b9a
Ultra3 Bots <dennistyg82@gmail.com>
2026-05-31 00:22:19 +0200

    Update referral.js

--- bot/commands/referral.js
+++ bot/commands/referral.js
@@ -1,15 +1,35 @@
-const { getReferral } = require("../engine/referralManager");
+const db = require("../../database/economy.sqlite");
+
+function generateCode(userId) {
+    return "ULTRA-" + userId.slice(-5);
+}
 
 module.exports = {
     name: "referral",
 
     execute(message) {
 
-        getReferral(message.author.id, (data) => {
+        const userId = message.author.id;
 
-            message.reply(
-                `🔗 Your referral code:\n\`${data.code}\`\n\nInvites: ${data.invites}\nPoints: ${data.points}`
-            );
-        });
+        db.get(
+            "SELECT * FROM referrals WHERE userId = ?",
+            [userId],
+            (err, row) => {
+
+                if (!row) {
+
+                    const code = generateCode(userId);
+
+                    db.run(
+                        "INSERT INTO referrals (userId, code) VALUES (?, ?)",
+                        [userId, code]
+                    );
+
+                    return message.reply(`🔗 Your referral code: **${code}**`);
+                }
+
+                message.reply(`🔗 Your referral code: **${row.code}**`);
+            }
+        );
     }
 };
