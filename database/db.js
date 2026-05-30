217c1d69f6da3d2b7ee23574439e7bb6ccb0f59c
Ultra3 Bots <dennistyg82@gmail.com>
2026-05-30 16:37:41 +0200

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,11 +1,18 @@
 const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("./database/app.db", (err) => {
-    if (err) {
-        console.error("❌ Database connection error:", err.message);
-    } else {
-        console.log("🧠 Main database connected");
-    }
+const db = new sqlite3.Database("./database/app.db");
+
+db.serialize(() => {
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS users (
+            id TEXT PRIMARY KEY,
+            tier TEXT DEFAULT 'FREE',
+            expiresAt INTEGER
+        )
+    `);
+
+    console.log("🧠 Main DB ready");
 });
 
 module.exports = db;
