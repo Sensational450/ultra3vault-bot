9a0f25b1c2e506cdce622bb72de98baecf65bbbb
Ultra3 Bots <dennistyg82@gmail.com>
2026-05-30 22:53:38 +0200

    Create referral.js

---
+++ bot/commands/referral.js
@@ -0,0 +1,18 @@
+const { getReferral } = require("../engine/referralManager");
+
+module.exports = {
+    name: "referral",
+
+    async execute(message) {
+
+        getReferral(message.author.id, (data) => {
+
+            message.reply(
+                `🤝 Your Referral Code:\n` +
+                `\`${data.code}\`\n\n` +
+                `📊 Invites: ${data.invites}\n` +
+                `⭐ Points: ${data.points}`
+            );
+        });
+    }
+};
