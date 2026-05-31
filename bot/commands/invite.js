f88f684249c9aef63217bf6ac81e742b3b28787e
Ultra3 Bots <dennistyg82@gmail.com>
2026-05-31 17:06:09 +0200

    Create invite.js

---
+++ bot/commands/invite.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: "invite",
+
+    async execute(message) {
+
+        return message.reply(
+`🔗 ULTRA3 INVITE SYSTEM
+
+Invite friends to grow the community.
+
+🎯 Benefits:
++ XP boost for every invite
++ Leaderboard ranking increase
++ Future VIP rewards
+
+👉 Keep building your trader rank in Ultra3`
+        );
+    }
+};
