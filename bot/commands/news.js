7e1c741240a8bf01518ff06419f5b2e760757d8f
Ultra3 Bots <dennistyg82@gmail.com>
2026-06-06 13:05:33 +0200

    Delete bot directory

--- bot/commands/news.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: "news",
-    async execute(message) {
-        return message.reply("📰 Latest crypto news loading...");
-    }
-};
--- bot/core/logger.js
+++
@@ -1,6 +0,0 @@
-function log(type, msg) {
-    const time = new Date().toISOString();
-    console.log(`[${type}] ${time} → ${msg}`);
-}
-
-module.exports = { log };
