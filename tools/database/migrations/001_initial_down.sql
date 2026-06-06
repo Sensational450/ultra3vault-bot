a280ff4ea4abd0c2ef1282cdafa84c79c0e300e7
Ultra3 Bots <dennistyg82@gmail.com>
2026-06-06 15:05:34 +0200

    Create 001_initial_down.sql

---
+++ tools/database/migrations/001_initial_down.sql
@@ -0,0 +1,7 @@
+-- ⏪ Rollback initial schema
+DROP TABLE IF EXISTS warnings;
+DROP TABLE IF EXISTS subscriptions;
+DROP TABLE IF EXISTS referrals;
+DROP TABLE IF EXISTS referral_codes;
+DROP TABLE IF EXISTS economy;
+DROP TABLE IF EXISTS users;
