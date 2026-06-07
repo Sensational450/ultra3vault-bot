b89ef04a57ea2e9e938483c2e7f5e0f8c758b04c
Ultra3 Bots <dennistyg82@gmail.com>
2026-06-07 20:52:41 +0200

    Update revokevip.js

--- commands/vip/revokevip.js
+++ commands/vip/revokevip.js
@@ -7,7 +7,6 @@ module.exports = {
     ],
   },
   async execute(interaction) {
-    const { eventBus } = interaction.client;
-    eventBus?.emit('command.revokevip', { interaction });
+    await interaction.client.orchestrator.onInteraction(interaction);
   },
 };
