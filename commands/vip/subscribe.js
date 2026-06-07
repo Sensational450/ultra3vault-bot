9b8ceaef5cc67af5cef42613eb9e9fee25d5708e
Ultra3 Bots <dennistyg82@gmail.com>
2026-06-07 20:41:16 +0200

    Create subscribe.js

---
+++ commands/vip/subscribe.js
@@ -0,0 +1,22 @@
+module.exports = {
+  data: {
+    name: 'subscribe',
+    description: '💎 Subscribe to a VIP tier',
+    options: [
+      {
+        name: 'tier',
+        type: 3,
+        description: 'Tier (vip / premium)',
+        required: true,
+        choices: [
+          { name: 'VIP', value: 'vip' },
+          { name: 'Premium', value: 'premium' },
+        ],
+      },
+    ],
+  },
+  async execute(interaction) {
+    const { eventBus } = interaction.client;
+    eventBus?.emit('command.subscribe', { interaction });
+  },
+};
