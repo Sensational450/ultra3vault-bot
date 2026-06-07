b0a71914a6441acfd5d1104093f57651e47a3133
Ultra3 Bots <dennistyg82@gmail.com>
2026-06-07 21:06:24 +0200

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,3 +1,7 @@
 module.exports = {
   data: { name: 'ping', description: '🏓 Check bot latency' },
+  async execute(interaction) {
+    // Forward to orchestrator (InfoAgent will handle)
+    await interaction.client.orchestrator.onInteraction(interaction);
+  },
 };
