1b51d9f7961183989be419d4da160282de36be00
Ultra3 Bots <dennistyg82@gmail.com>
2026-06-07 20:51:47 +0200

    Update renew.js

--- commands/vip/renew.js
+++ commands/vip/renew.js
@@ -1,7 +1,6 @@
 module.exports = {
   data: { name: 'renew', description: '🔄 Renew your subscription' },
   async execute(interaction) {
-    const { eventBus } = interaction.client;
-    eventBus?.emit('command.renew', { interaction });
+    await interaction.client.orchestrator.onInteraction(interaction);
   },
 };
