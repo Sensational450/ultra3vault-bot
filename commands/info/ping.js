30f04ef069732c4fcd7ef94e14fccf0d5ca2f92d
Ultra3 Bots <dennistyg82@gmail.com>
2026-06-07 16:33:06 +0200

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -3,7 +3,9 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = {
   data: { name: 'ping', description: '🏓 Check bot latency' },
   async execute(interaction) {
+    // Defer the reply to avoid timeout (important!)
     await interaction.deferReply();
+
     const latency = interaction.client.ws.ping;
     const embed = new EmbedBuilder()
       .setTitle('🏓 Pong!')
