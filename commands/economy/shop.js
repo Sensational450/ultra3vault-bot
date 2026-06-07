fed4c081a456cf81832bcd6c4d2497a0daa68fa6
Ultra3 Bots <dennistyg82@gmail.com>
2026-06-07 16:34:00 +0200

    Update shop.js

--- commands/economy/shop.js
+++ commands/economy/shop.js
@@ -3,10 +3,14 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = {
   data: { name: 'shop', description: '🛒 View items for sale' },
   async execute(interaction) {
-    // No deferral needed – reply immediately
+    // No need to defer – reply immediately
     const embed = new EmbedBuilder()
       .setTitle('🛒 Shop')
-      .setDescription('**VIP Role** - 5000 coins\nAccess to VIP channels\n\n**Lottery Ticket** - 100 coins\nEnter weekly lottery\n\n**Red Name Color** - 2000 coins\nCustom role color')
+      .setDescription(
+        '**VIP Role** - 5000 coins\nAccess to VIP channels\n\n' +
+        '**Lottery Ticket** - 100 coins\nEnter the weekly lottery\n\n' +
+        '**Red Name Color** - 2000 coins\nCustom role color'
+      )
       .setColor(0xffaa00);
     await interaction.reply({ embeds: [embed] });
   },
