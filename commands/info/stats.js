fed76da47dfa172d3172e5a4ce3041f18bd943cd
Ultra3 Bots <dennistyg82@gmail.com>
2026-06-07 17:17:28 +0200

    Update stats.js

--- commands/info/stats.js
+++ commands/info/stats.js
@@ -1,7 +1,3 @@
 module.exports = {
   data: { name: 'stats', description: '📊 Show bot statistics' },
-  async execute(interaction) {
-    await interaction.deferReply();
-    await interaction.editReply('📊 Bot is running!');
-  },
 };
