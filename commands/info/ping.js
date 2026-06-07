f126e94e7658ccd46d3a0b0806ce16c23bf500b0
Ultra3 Bots <dennistyg82@gmail.com>
2026-06-07 17:17:06 +0200

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,7 +1,3 @@
 module.exports = {
   data: { name: 'ping', description: '🏓 Check bot latency' },
-  async execute(interaction) {
-    await interaction.deferReply(); // MUST defer first
-    await interaction.editReply('🏓 Pong!');
-  },
 };
